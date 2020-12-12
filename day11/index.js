676ebacb9e87559331f62ad1f57a45559fb6fd2d
Mrinal Virnave <mrinal.virnave@transactcampus.com>
2020-12-11 23:29:34 -0800

    day12 done

--- day11/index.js
+++ day11/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
+const async = require('async');
+const { exit } = require('process');
 
 const rl = readline.createInterface({
   input: fs.createReadStream('./day11/input.txt'),
@@ -20,25 +22,58 @@ var numcolumns, numrows;
 rl.on('close', () => {
   numrows = table.length;
   numcolumns = table[0].length;
-  var filledSeatCount = iterate(table,0);
+  result = iterate(table, 0);
 });
 
-function iterate(tableArray, round) {
+
+let iterate = async function (tableArray, round) {
 
   var prevArray = tableArray;
-  var newArray = new Array (numrows).fill(new Array(numcolumns).fill(null));
+  var newArray = new Array(numrows).fill(new Array(numcolumns).fill(null));
 
-  prevArray.forEach(async function (rowArray, rowNum, seatArray) {
-    rowArray.forEach(async function (cell, colNum ) {
-      console.log(`RowNum:${rowNum}, ColNum:${colNum}`);
-      newArray[rowNum][colNum] = await getNewState (cell, seatArray, rowNum, colNum);
-      return;
-    });    
-  });
+  async.forEachOf(prevArray, (rowArray, rowNum) => {
+    async.forEachOf(rowArray, (cell, colNum) => {
+      newArray[rowNum, colNum] = getNewState(prevArray, rowNum, colNum);
+    })
+  }, function (err) { console.log(err); });
 
- console.log(newArray[1][1]);
+  console.log(newArray);
 }
 
-let getNewState = async function (cell, seatArray, rowNum, colNum) {
+
+let getNewState = function (seatArray, rowNum, colNum) {
+    
+  if (seatArray[rowNum][colNum] == "#" && getSurrounding(seatArray, rowNum, colNum) > 3) {
+    return "L";
+  } 
+  if (seatArray[rowNum][colNum] == "L" && getSurrounding(seatArray, rowNum, colNum) == 0) {
+    return "#";
+  } 
+  
   return seatArray[rowNum][colNum];
+}
+
+let getSurrounding = function (seatArray, rowNum, colNum) {
+  var filledSeatCount = getFilledSeatCount(seatArray, rowNum-1, colNum-1) +
+                        getFilledSeatCount(seatArray, rowNum-1, colNum) +
+                        getFilledSeatCount(seatArray, rowNum-1, colNum+1) +
+                        getFilledSeatCount(seatArray, rowNum, colNum-1) +
+                        getFilledSeatCount(seatArray, rowNum, colNum) +
+                        getFilledSeatCount(seatArray, rowNum, colNum+1) +
+                        getFilledSeatCount(seatArray, rowNum+1, colNum-1) +
+                        getFilledSeatCount(seatArray, rowNum+1, colNum) +
+                        getFilledSeatCount(seatArray, rowNum+1, colNum+1);
+  
+  return filledSeatCount;
+}
+
+let getFilledSeatCount = function (seatArray, rowNum, colNum) {
+  try {
+    return (seatArray[rowNum][colNum] == "#" ? 1 : 0)
+    
+  } catch (error) {
+    console.log (`Row: ${rowNum}, Col: ${colNum}`);
+    console.log (JSON.stringify(error));
+    return 0;
+  }
 }
---
+++ day12/index-p1.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const readline = require('readline');
+const math = require('mathjs');
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('input.txt'),
+  output: process.stdout,
+  terminal: false
+});
+
+var location = [0, 0];
+var heading = [1, 0];
+var rotations = {
+  L90: [[0, 1], [-1, 0]],
+  R270: [[0, 1], [-1, 0]],
+  R90: [[0, -1], [1, 0]],
+  L270: [[0, -1], [1, 0]],
+  R180: [[-1, 0], [0, -1]],
+  L180: [[-1, 0], [0, -1]]
+};
+var translate = {
+  N: [0, 1],
+  E: [1, 0],
+  S: [0, -1],
+  W: [-1, 0]
+}
+var count = 0;
+
+rl.on('line', (line) => {
+
+  var command = line.substr(0, 1);
+  if (command == 'R' || command == 'L') {
+    heading = math.multiply(heading, rotations[line]);
+  }
+  else if (command == 'F') {
+    var val = line.substring(1);
+    var move = math.multiply(heading, +val);
+    location = math.add(location, move);
+  }
+  else {
+    var val = line.substring(1);
+    var move = math.multiply(translate[command], +val);
+    location = math.add(location, move);
+  }
+  count++;
+  console.log(`${count}- ${line} - ${location}`);
+});
+
+rl.on('close', () => {
+  console.log(location);
+  console.log(Math.abs(location[0])+Math.abs(location[1]));
+});
